22845105ecb1cab146b5d7621f42551af0390f7c
The29thElement <element29.11.63@gmail.com>
2026-05-19 16:19:27 -0500

    too many

--- src/main.py
+++ src/main.py
@@ -14,9 +14,13 @@ class Main:
 
     def mainloop(self):
         
+        screen = self.screen
+        game = self.game
+
         while True:
 
-            self.game.show_bg(self.screen)
+            game.show_bg(screen)
+            game.show_pieces(self.screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- src/piece.py
+++ src/piece.py
@@ -1,4 +1,5 @@
 import math
+import os
 
 class Piece:
 
@@ -9,12 +10,20 @@ class Piece:
         value_sign = 1 if color == 'white' else -1
         self.value = value * value_sign
 
+        self.moves = []
+        self.moved = False
+
         self.texture = texture
         self.set_texture()
         self.texture_rect = texture_rect
 
-    def set_texture(self):
-        pass
+    def set_texture(self, size = 80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png'
+        )
+
+    def add_moves(self, move):
+        self.moves.append(move)
 
 class Pawn(Piece):
     
