aea4d1b1d51ce8859d0f6738172132e93abbccd6
The29thElement <element29.11.63@gmail.com>
2026-05-23 14:52:09 -0500

    1:30:47

--- src/main.py
+++ src/main.py
@@ -25,22 +25,32 @@ class Main:
             game.show_pieces(self.screen)
 
             for event in pygame.event.get():
-
+                
+                #click
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     dragger.update_mouse(event.pos)
                     
                     clicked_row = dragger.mouseY // SQSIZE
                     clicked_col = dragger.mouseX // SQSIZE
 
+                     #check for a piece
                     if board.squares[clicked_row][clicked_col].has_piece():
                         piece = board.squares[clicked_row][clicked_col]
+                        dragger.save_initial(event.pos)
+                        dragger.drag_piece(piece)
 
+                #mouse motion
                 elif event.type == pygame.MOUSEMOTION:
-                    pass
 
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+                        dragger.update_blit(screen)
+
+                #mouse up
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    pass
+                    dragger.undrag_piece()
 
+                #quit the game
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
